c20b33c577694cd7e3c91884b9fdde59c210927e
John <80285394@qq.com>
2021-06-29 17:27:06 +0800

    fix.

--- modules/seo_station/src/Entity/Station.php
+++ modules/seo_station/src/Entity/Station.php
@@ -404,10 +404,10 @@ class Station extends ContentEntityBase implements StationInterface {
         'weight' => 0,
         'label' => 'inline',
       ])
-//      ->setDisplayOptions('form', [
-//        'type' => 'options_select',
-//        'weight' => 0,
-//      ])
+      ->setDisplayOptions('form', [
+        'type' => 'options_select',
+        'weight' => 0,
+      ])
       ->setDisplayConfigurable('form', TRUE)
       ->setDisplayConfigurable('view', TRUE);
 //企业简介句子(site_intro)
