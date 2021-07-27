3efed738e0736c0e59a1560b97d5d6fd4509b9cb
John <80285394@qq.com>
2021-07-27 15:04:31 +0800

    Refactor.

--- modules/seo_textdata/src/TextdataManager.php
+++ modules/seo_textdata/src/TextdataManager.php
@@ -363,4 +363,69 @@ class TextdataManager implements TextdataManagerInterface {
 
     return $entity;
   }
+
+
+  public function generateNode($data) {
+    $station = $data['station'];
+    // TODO， 暂时只做一个新闻类型的网站文章数据.
+    $node_storage = $this->entityTypeManager->getStorage('node');
+
+    $body = $this->getBody($station);
+    if (empty($body)) {
+      return;
+    }
+    // 找标题的原则是body的标题在title库里面找到了就用，没找到就使用自己的标题！如果自己的标题为空，则随机调用标题库里面的标题
+    //    $title = $this->getTitle($body[0], $station);
+    $title = \Drupal::service('seo_textdata.manager')->getTitle($body[0], $station);
+    if (empty($title)) {
+      return;
+    }
+    try {
+      // 初始化node的值, 及Site name.
+      [$site_name, $tkdb_values] = $this->getTkdbValues($data, $station);
+
+      // 这里会创建N多，但站群网站却只要几个，矛盾点，待优化
+      $taxonomies = $this->getTaxonomyValues($station);
+
+      // 提取文章分类到标题后缀
+      // 构造一个tid的数组.
+      $rand_tids = [];
+      if (!empty($taxonomies)) {
+        $rand_tids = array_rand($taxonomies, 5);
+      }
+      if (!is_array($rand_tids)) {
+        $rand_tids[] = $rand_tids;
+      }
+      $rs_rand_tid = reset($rand_tids);
+      $term = $taxonomies[$rs_rand_tid];
+      $tkdb_values = $this->appendTaxonomy2Title($term, $tkdb_values, $site_name);
+
+      $values = [
+        'type' => 'article',
+        'title' => is_array($title) ? mb_substr($title[0], 0, 60) : mb_substr($title, 0, 60),
+        //        'field_image' => '',
+        'body' => [
+          'value' => $body[1],
+          'summary' => mb_substr($body[1], 0, 100),
+          'format' => 'basic_html',
+        ],
+        'path' => '/'.$data['replacement'],//Alias
+        'field_metatag' => [
+          'value' => serialize($tkdb_values),
+        ],
+        //TODO, Add taxonomy
+        'field_tags' => $rand_tids, //$tids,
+        'field_station' => $station->id(),
+      ];
+      // 创建该别名的文章数据.
+      $node = $node_storage->create(['type' => 'article']);
+      foreach ($values as $key => $val) {
+        $node->set($key, $val);
+      }
+      $node->save();
+    }
+    catch (\Exception $e) {
+      \Drupal::messenger()->addWarning($e);
+    }
+  }
 }
