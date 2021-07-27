609f2e934cb609886216cfe9e9fc02b343db7138
John <80285394@qq.com>
2021-07-27 16:40:32 +0800

    Refactor.

--- modules/seo_textdata/src/TextdataManager.php
+++ modules/seo_textdata/src/TextdataManager.php
@@ -25,31 +25,20 @@ class TextdataManager implements TextdataManagerInterface {
    */
   public function getWebnameByStationAddress($theme_info) {
     $sitename = '';
-    $host = \Drupal::request()->getHost();
-    $storage = $this->entityTypeManager->getStorage('seo_station_address');
-    $query = $storage->getQuery();
-    $query->condition('domain', $host); // 不能有重复的, 网站名称固化到完整域名上
-    $ids = $query->execute(); // 不能有多个.
-    if (!empty($ids)) {
-      $entity = $storage->load(reset($ids));
-      $sitename = $entity->webname->value;
+    // 网站名称
+    $textdata_storage = \Drupal::entityTypeManager()->getStorage('seo_textdata');
+    $textdata_query = $textdata_storage->getQuery();
+    $textdata_query->condition('type', 'webname');
+    $textdata_query->condition('model.entity.config_dir', $theme_info['seo_theme']);
+    $ids = $textdata_query->execute();
+    if (empty($ids)) {
+      return '';
     }
-    else {
-      // 网站名称
-      $textdata_storage = \Drupal::entityTypeManager()->getStorage('seo_textdata');
-      $textdata_query = $textdata_storage->getQuery();
-      $textdata_query->condition('type', 'webname');
-      $textdata_query->condition('model.entity.config_dir', $theme_info['seo_theme']);
-      $ids = $textdata_query->execute();
-      if (empty($ids)) {
-        return '';
-      }
-      $textdata = $textdata_storage->load($ids[array_rand($ids, 1)]);
+    $textdata = $textdata_storage->load($ids[array_rand($ids, 1)]);
 
-      if (!empty($textdata->attachment->target_id)) {
-        $webnames = getTextdataArrayFromUri($textdata->attachment->entity->getFileUri());
-        $sitename = $webnames[array_rand($webnames, 1)];
-      }
+    if (!empty($textdata->attachment->target_id)) {
+      $webnames = getTextdataArrayFromUri($textdata->attachment->entity->getFileUri());
+      $sitename = $webnames[array_rand($webnames, 1)];
     }
 
     return $sitename;
